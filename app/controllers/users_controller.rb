f2523947f9859016f49d8070bb5fe099ed941572
fumi0 <fu-m1110@i.softbank.jp>
2016-03-15 04:02:22 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
     
-     def create
+    def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to Sample App!"
@@ -17,11 +17,25 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
-  private
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile update"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+    
+ private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, )
   end
 end
